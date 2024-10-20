43b7e984b820aaf8be8b21d5a08a9940d68756c5
ValentaSaputra <valentaofficial@gmail.com>
2024-10-20 13:09:59 +0700

    change logo index.jsx

--- src/components/Elements/Logo/index.jsx
+++ src/components/Elements/Logo/index.jsx
@@ -1,17 +1,16 @@
+import React from 'react'
 
-
-const Logo = () => {
+const index = () => {
   return (
-    <div>
+    <>
     {/* logo start */}
-      <div className="flex justify-center font-poppins tracking-wide text-primary text-4xl">
-        <span className="font-bold ">FINE</span>bank
-        <span className="font-bold">.IO</span>
-      </div>
-      {/* logo end */}
+<div className="flex justify-center font-poppins tracking-wide text-primary text-4xl">
+<span className="font-bold ">FINE</span>bank
+<span className="font-bold">.IO</span>
+</div>
+{/* logo end */}
+    </>
+  )
+}
 
-      </div>
-  );
-};
-
-export default Logo
+export default index
