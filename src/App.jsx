b800d84d795e8448fe34b11973616aa22d42f2b8
ValentaSaputra <valentaofficial@gmail.com>
2024-10-20 12:18:18 +0700

    fix error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import SignInPage from "./pages/signIn";
-import SignUpPage from "./pages/signUp";
-import ErrorRoute from "./pages/errorRoute";
+import SignInPage from "./Pages/signIn";
+import SignUpPage from "./Pages/signUp";
+import ErrorRoute from "./Pages/errorRoute";
 
 const App = () => {
   const myRouter = createBrowserRouter([
